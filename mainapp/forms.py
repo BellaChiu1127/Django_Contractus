3af4c9ee635a50d0b1d3a38196348973b5a93d85
Bella <Bella112798@Gmail.com>
2019-08-10 10:37:03 +0800

    套用Contract us 的css

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -5,3 +5,13 @@ class ContractUsForm(forms.ModelForm):
     class Meta:
         model = ContractUs
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(ContractUsForm, self).__init__(*args, **kwargs)
+
+        # sets the placeholder key/value in the attrs for a widget
+        # when the form is instantiated (so the widget already exists)
+        self.fields['user_name'].widget.attrs['placeholder'] = 'Your Name'
+        self.fields['user_email'].widget.attrs['placeholder'] = 'Your Email'
+        self.fields['user_subject'].widget.attrs['placeholder'] = 'Subject'
+        self.fields['user_message'].widget.attrs['placeholder'] = 'Message'
--- mainapp/models.py
+++ mainapp/models.py
@@ -10,8 +10,8 @@ class SignAdmin(admin.ModelAdmin):
     list_display = ('id','user_id')
 class ContractUs(models.Model):
     user_name = models.CharField(max_length=5)
-    user_email = models.CharField(max_length=30)
+    user_email = models.EmailField(max_length=30)
     user_subject = models.CharField(max_length=30)
-    user_message = models.CharField(max_length=1000)
+    user_message = models.TextField(max_length=800)
     def __str__(self):
         return self.user_name
